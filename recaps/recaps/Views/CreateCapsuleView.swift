a12426012c37daed3f9b149b5d81b74f37362619
Richard Fagundes Rodrigues <richardsiacc@Richards-MacBook-Pro.local>
2025-11-21 16:53:15 -0300

    Low fidelity screens implemented, waiting for logics and high fidelity

---
+++ recaps/recaps/Views/CreateCapsuleView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateCapsuleView.swift
+//  recaps
+//
+//  Created by Richard Fagundes Rodrigues on 21/11/25.
+//
+
+import SwiftUI
+
+struct CreateCapsuleView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    CreateCapsuleView()
+}
