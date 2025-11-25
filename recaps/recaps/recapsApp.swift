cc070d263705916f3adf505b96f4de46763b1c12
Leonel <leonelfhernandez@outlook.com>
2025-11-25 15:08:28 -0300

    Fixed the NavigationStack

--- recaps/recaps/recapsApp.swift
+++ recaps/recaps/recapsApp.swift
@@ -11,9 +11,7 @@ import SwiftUI
 struct recapsApp: App {
     var body: some Scene {
         WindowGroup {
-            NavigationStack{
-                HomeRecapsView()
-            }
+            HomeRecapsView()
         }
     }
 }
