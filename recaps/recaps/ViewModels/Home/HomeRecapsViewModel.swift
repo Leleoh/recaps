c201995be96a58efd5affc665a61a58a172bf1dc
Leonel <leonelfhernandez@outlook.com>
2025-11-25 16:49:29 -0300

    fixed the files and the size of the code generated to join a capsule

--- recaps/recaps/ViewModels/Home/HomeRecapsViewModel.swift
+++ recaps/recaps/ViewModels/Home/HomeRecapsViewModel.swift
@@ -1,8 +1,96 @@
 //
-//  HomeRecapsViewModel.swift
+//  CreatingCapsuleTestViewModel.swift
 //  recaps
 //
-//  Created by Richard Fagundes Rodrigues on 19/11/25.
+//  Created by Ana Carolina Poletto on 24/11/25.
 //
 
-import Foundation
+import SwiftUI
+import AuthenticationServices
+import CloudKit
+
+@Observable
+class HomeRecapsViewModel {
+    private let userService = UserService()
+    private let capsuleService = CapsuleService()
+    
+    func generateCode() -> String {
+        let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+        return String((0..<5).map { _ in chars.randomElement()! })
+    }
+    
+    func creatingCapsule(code: String, name: String, offensive: Int) async {
+        do {
+            let capsule = Capsule(
+                id: UUID(),
+                code: code,
+                submissions: [],
+                name: name,
+                createdAt: Date(),
+                offensive: offensive,
+                lastSubmissionDate: Date(),
+                validOffensive: true,
+                lives: 3,
+                members: [userService.getUserId()],
+                ownerId: userService.getUserId(),
+                status: .inProgress
+            )
+
+            print("Criando cápsula")
+
+            let newCapsuleID = try await capsuleService.createCapsule(capsule: capsule)
+
+            let currentUser = try await userService.getCurrentUser()
+
+            var userCapsules = currentUser.capsules
+            userCapsules.append(newCapsuleID)
+
+            print(capsule)
+            print("Usuário:", currentUser)
+
+            try await userService.updateUser(currentUser, capsules: userCapsules)
+
+            let user = try await userService.getCurrentUser()
+            print("User atualizado:", user)
+
+        } catch {
+            print("Erro ao criar a cápsula", error)
+        }
+    }
+
+    
+    
+    
+    func joinCapsule(code: String) async throws {
+        let allCapsules = try await capsuleService.fetchAllCapsulesWithoutSubmissions()
+        
+        var user = try await userService.getCurrentUser()
+        
+        guard var capsule = allCapsules.first(where: { $0.code == code }) else {
+            print("Nenhuma cápsula encontrada com esse código.")
+            return
+        }
+        
+        if capsule.members.contains(user.id) {
+            print("Usuário já é membro desta cápsula.")
+            return
+        }
+        
+        if user.capsules.contains(capsule.id) {
+            print("Cápsula já está na lista do usuário.")
+            return
+        }
+        
+        capsule.members.append(user.id)
+        try await capsuleService.updateCapsule(capsule: capsule)
+        
+        user.capsules.append(capsule.id)
+        _ = try await userService.updateUser(user)
+        
+        print("Usuário entrou na cápsula:", capsule.code)
+        print("User:", user)
+        print("Capsule:", capsule)
+    }
+    
+    
+}
