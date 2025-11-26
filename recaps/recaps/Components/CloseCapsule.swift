4536518ce6b1122014a959d69246a76c84f12bbb
AnaPolettoo <anapoletto@MacBook-Pro-de-Ana.local>
2025-11-26 10:35:41 -0300

    Componente de capsulas abertas e fechadas

--- recaps/recaps/Components/CloseCapsule.swift
+++ recaps/recaps/Components/CloseCapsule.swift
@@ -9,13 +9,56 @@ import Foundation
 import SwiftUI
 
 struct CloseCapsule: View {
+    var capsule: Capsule
+
     var body: some View {
-        Image(.closeCapsule)
-            .frame(width: 313, height: 211)
+        ZStack {
+            let lastThree = Array(capsule.submissions.suffix(3))
+
+            ZStack {
+                ForEach(Array(lastThree.enumerated()), id: \.1.id) { index, submission in
+                    SubmissionView(submission: submission)
+                        .offset(x: CGFloat(-2 * index), y: CGFloat(-18 * index))
+                        .opacity(0.9 - Double(index) * 0.25)
+                }
+            }
+            .offset(y: 20)
+            Image(.closeCapsule)
+                .frame(width: 313, height: 211)
+                .opacity(0.8)
+        } 
     }
 }
 
 
+
+
+
+
 #Preview {
-    CloseCapsule()
+    let url = URL(string: "https://picsum.photos/300/200")!
+
+
+    let testSubmissions = [
+        Submission(id: UUID(), imageURL: url, description: "Primeira", authorId: "1", date: Date(), capsuleID: UUID()),
+        Submission(id: UUID(), imageURL: url, description: "Segunda", authorId: "2", date: Date(), capsuleID: UUID()),
+        Submission(id: UUID(), imageURL: url, description: "Terceira", authorId: "3", date: Date(), capsuleID: UUID())
+    ]
+
+    return CloseCapsule(
+        capsule: Capsule(
+            id: UUID(),
+            code: "F5GX3",
+            submissions: testSubmissions,
+            name: "Academy",
+            createdAt: Date(),
+            offensive: 50,
+            lastSubmissionDate: Date(),
+            validOffensive: false,
+            lives: 3,
+            members: [],
+            ownerId: " ",
+            status: .inProgress
+        )
+    )
 }
--- recaps/recaps/Components/OpenCapsule.swift
+++ recaps/recaps/Components/OpenCapsule.swift
@@ -10,12 +10,56 @@ import SwiftUI
 
 struct OpenCapsule: View {
     var isPink: Bool = true
+    var capsule: Capsule
     
     var body: some View {
-        Image(isPink ? .openCapsulePink : .openCapsuleGreen)
+        ZStack {
+            Image(isPink ? .backOpenCapsulePink : .backOpenCapsuleGreen)
+                .frame(width: 313, height: 211)
+                .opacity(0.8)
+                .offset(x: CGFloat(0), y: CGFloat(-56))
+            let lastThree = Array(capsule.submissions.suffix(3))
+            ZStack {
+                ForEach(Array(lastThree.enumerated()), id: \.1.id) { index, submission in
+                    SubmissionView(submission: submission, height: 80, width: 110)
+                        .offset(x: CGFloat(-1 * index), y: CGFloat(-10 * index))
+                        .opacity(0.9 - Double(index) * 0.25)
+                }
+            }
+
+            Image(isPink ? .openCapsulePink : .openCapsuleGreen)
+                .frame(width: 313, height: 211)
+                .opacity(0.8)
+        }
     }
 }
 
 #Preview {
-    OpenCapsule()
+    let url = URL(string: "https://picsum.photos/300/200")!
+
+
+    let testSubmissions = [
+        Submission(id: UUID(), imageURL: url, description: "Primeira", authorId: "1", date: Date(), capsuleID: UUID()),
+        Submission(id: UUID(), imageURL: url, description: "Segunda", authorId: "2", date: Date(), capsuleID: UUID()),
+        Submission(id: UUID(), imageURL: url, description: "Terceira", authorId: "3", date: Date(), capsuleID: UUID())
+    ]
+
+    return OpenCapsule(
+        isPink: false,
+        capsule: Capsule(
+            id: UUID(),
+            code: "F5GX3",
+            submissions: testSubmissions,
+            name: "Academy",
+            createdAt: Date(),
+            offensive: 50,
+            lastSubmissionDate: Date(),
+            validOffensive: false,
+            lives: 3,
+            members: [],
+            ownerId: " ",
+            status: .inProgress
+        )
+    )
 }
+
