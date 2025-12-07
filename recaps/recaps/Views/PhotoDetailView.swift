27b6cb32008928caef48326d1e5f5c89a184660e
AnaPolettoo <anapoletto@MacBook-Pro-de-Ana-2.local>
2025-12-07 11:32:27 -0300

    ViewPhotos Detaisl

--- recaps/recaps/Views/PhotoDetailView.swift
+++ recaps/recaps/Views/PhotoDetailView.swift
@@ -4,25 +4,98 @@
 //
 //  Created by Ana Poletto on 03/12/25.
 //
+
 import SwiftUI
 
 struct PhotoDetailView: View {
     let submission: Submission
+    @State private var viewModel = PhotoDetailsViewModel()
+    @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        VStack {
-            AsyncImage(url: submission.imageURL) { image in
-                image
-                    .resizable()
-                    .scaledToFit()
-            } placeholder: {
-                ProgressView()
+        ZStack {
+            Color.black.opacity(0.6)
+                .ignoresSafeArea()
+                .onTapGesture { dismiss() }
+            
+            VStack(alignment: .leading, spacing: 30) {
+                VStack {
+                    AsyncImage(url: submission.imageURL) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                            .rotationEffect(.degrees(-2))
+                            .shadow(radius: 20)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    
+                    VStack(alignment: .trailing, spacing: 4) {
+                        Text(submission.description ?? "")
+                            .font(.coveredByYourGraceSignature)
+                        
+                        Text(viewModel.formatDate(submission.date))
+                            .font(.coveredByYourGraceSignature)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+                
+                Text(viewModel.userName)
+                    .font(.coveredByYourGraceTitle)
             }
-            Text(submission.description ?? "")
-            Text(submission.date, style: .date)
+            .padding(.horizontal, 44)
+            
+            VStack {
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    if let image = viewModel.shareableImage {
+                        let swiftUIImage = Image(uiImage: image)
+                        
+                        ShareLink(
+                            item: swiftUIImage,
+                            preview: SharePreview("Achievement!", image: swiftUIImage)
+                        )  {
+                            Image(systemName: "square.and.arrow.up")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                                .foregroundStyle(.white)
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .frame(width: 48, height: 48)
+                        .tint(.black)
+                        .applyLiquidGlass(shape: RoundedRectangle(cornerRadius: 100))
+                        .padding(.trailing, 26)
+                        .padding(.bottom, 26)
+                    }
+                }
+            }
         }
-        .padding()
-        .navigationTitle("Photo")
-        .navigationBarTitleDisplayMode(.inline)
+        .task {
+            if let name = await viewModel.getUser(id: submission.authorId) {
+                viewModel.userName = name
+            }
+            
+            if let url = submission.imageURL {
+                await viewModel.loadShareableImage(from: url)
+            }
+        }
     }
+}
+
+#Preview {
+    let url = URL(string: "https://picsum.photos/300/200")!
+    
+    let testSubmissions = Submission(
+        id: UUID(),
+        imageURL: url,
+        description: "Primeira",
+        authorId: "1",
+        date: Date(),
+        capsuleID: UUID()
+    )
+    
+    PhotoDetailView(submission: testSubmissions)
 }
