c4f85c86b7b283367c300b18687281fa20751e72
AnaPolettoo <anapoletto@MacBook-Pro-de-Ana-2.local>
2025-12-09 10:11:05 -0300

    Imagem de fundo

--- recaps/recaps/Views/AuthenthicationView.swift
+++ recaps/recaps/Views/AuthenthicationView.swift
@@ -12,28 +12,34 @@ struct AuthenthicationView: View {
     @State var viewModel = AuthenthicationViewModel()
     
     var body: some View {
-        VStack(spacing: 62) {
-            Image(.loginLock)
+        ZStack{
+            Image(.backgroundPNG)
+                .resizable()
+                .ignoresSafeArea()
             
-            VStack(spacing: 12){
-                Text("Recapsule")
-                    .font(.custom("CoveredByYourGrace", size: 48))
+            VStack(spacing: 62) {
+                Image(.loginLock)
                 
-                Text("Unlock memories")
-                    .font(.title3)
-            }
-            
-            SignInWithAppleButton(.continue) { request in
-                request.requestedScopes = [.email, .fullName]
-            } onCompletion: { result in
-                Task { @MainActor in
-                    await viewModel.handleAuthResult(result)
+                VStack(spacing: 12){
+                    Text("Recapsule")
+                        .font(.custom("CoveredByYourGrace", size: 48))
+                    
+                    Text("Unlock memories")
+                        .font(.title3)
                 }
+                
+                SignInWithAppleButton(.continue) { request in
+                    request.requestedScopes = [.email, .fullName]
+                } onCompletion: { result in
+                    Task { @MainActor in
+                        await viewModel.handleAuthResult(result)
+                    }
+                }
+                .signInWithAppleButtonStyle(.white)
+                .cornerRadius(32)
+                .frame(height: 54)
+                .padding()
             }
-            .signInWithAppleButtonStyle(.white)
-            .cornerRadius(32)
-            .frame(height: 54)
-            .padding()
         }
     }
 }
